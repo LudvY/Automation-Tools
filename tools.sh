135105e6d0e8ed95dc7a3771eb92f7fa66995526
LudvY <98172945+LudvY@users.noreply.github.com>
2022-08-27 15:55:06 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -2,6 +2,8 @@
 
 sudo apt update 
 sudo apt upgrade 
+sudo apt install python3
+sudo apt install python3-pip
 
 echo -e "\n [+] Insalling Golang : "
 sudo apt install golang -y
@@ -15,7 +17,11 @@ echo -e "\n [+] Insalling Amass for Ubuntu w snapstore: "
 sudo apt install snapd && sudo sudo snap install amass
 echo -e "\n [+] Insalling anew: "
 sudo go install -v github.com/tomnomnom/anew@latest
-echo -e "\n --------------------------Done INstalling for any error try to install them manually ------------------------------------"
+echo -e "\n [+] Insalling Linkfinder: "
+git clone https://github.com/GerbenJavado/LinkFinder.git ; cd LinkFinder ; python setup.py install
+echo -e "\n [+] Insalling SecretFinder: "
+git clone https://github.com/m4ll0k/SecretFinder.git secretfinder ; cd secretfinder ; pip install -r requirements.txt
+echo -e "\n --------------------------Done Installing required Tools for any error try to install them manually ------------------------------------"
 
 
 
